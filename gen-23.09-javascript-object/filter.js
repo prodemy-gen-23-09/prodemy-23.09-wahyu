276bc5f939f44609e541072ca8cc0cb915ee8ffc
wahyuharyo <haryo.wahyu17@gmail.com>
2023-11-28 12:54:37 +0700

    add folder gen-23.09-javascript-object

---
+++ gen-23.09-javascript-object/filter.js
@@ -0,0 +1,59 @@
+const product = [
+  {
+    name: "Hot Coffee",
+    catagory: "coffee",
+    price: 15000,
+    stok: 50,
+  },
+
+  {
+    name: "Hot Espresso",
+    catagory: "coffee",
+    price: 25000,
+    stok: 20,
+  },
+
+  {
+    name: "Green Tea Blended Creame",
+    catagory: "tea",
+    price: 13000,
+    stok: 100,
+  },
+
+  {
+    name: "Banana Bites",
+    catagory: "snack",
+    price: 9000,
+    stok: 100,
+  },
+  {
+    name: "Chocolate Grande",
+    catagory: "chocolate",
+    price: 19000,
+    stok: 75,
+  },
+
+  {
+    name: "BBQ Chicken Wings",
+    catagory: "snack",
+    price: 13000,
+    stok: 20,
+  },
+];
+
+// const snackProduct = product.filter((product) => product.catagory === "snack");
+// console.log(snackProduct);
+
+// const filterStok = product.filter((product) => product.stok > 20);
+// console.log(filterStok);
+
+// const nonCoffee = product.filter((product) => product.catagory != "coffee");
+// console.log(nonCoffee);
+
+// const a = product[0];
+// a.price = 20000;
+
+// const coffeeProduct = product.filter(
+//   (product) => product.catagory === "coffee"
+// );
+// console.log(coffeeProduct);
---
+++ gen-23.09-javascript-object/newObject.js
@@ -0,0 +1,10 @@
+const mahasiswa1 = {
+  name: "Wahyu Haryo",
+  age: 20,
+  prodi: "fisika",
+};
+
+const a = mahasiswa1;
+a.age = 23;
+
+console.log(mahasiswa1);
