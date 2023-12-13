3d55b5a2a3422d0870176b9838b1bda40e04afbc
wahyuharyo <haryo.wahyu17@gmail.com>
2023-12-13 23:06:03 +0700

    upload folder gen-2309-redux-toolkit

---
+++ gen-2309-redux-toolkit/src/layout/Header.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import Logo from "/src/assets/ae-removebg-preview.png";
+
+function Header() {
+  return (
+    <header className="navbar-container bg-white top-0 flex sticky py-3">
+      <div className="flex items-center pl-[80px]">
+        <Link to="/home">
+          <img src={Logo} alt="Logo" className="h-9" />
+        </Link>
+      </div>
+      <nav className="flex items-center ml-auto pr-[80px]">
+        <ul className="flex">
+          <li>
+            <Link
+              to="/home"
+              className="text-hijau text-lg font-semibold px-4 py-2 no-underline transition duration-300 hover:text-black"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <a
+              href="/listProduct"
+              className="text-hijau text-lg font-semibold px-4 py-2 no-underline transition duration-300 hover:text-black"
+            >
+              Menu
+            </a>
+          </li>
+          <li>
+            <a
+              href="/cart"
+              className="text-hijau text-lg font-semibold px-4 py-2 no-underline transition duration-300 hover:text-black"
+            >
+              Cart
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
